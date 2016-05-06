92412fd8fe0150c66705bce4dcc5b1b55f21df5a
Phil Manijak <github@exclsr.com>
2016-05-05 20:44:24 -0700

    Use templating engine (Pug)
    ... formerly known as Jade.

--- app/index.js
+++ app/index.js
@@ -1,10 +1,17 @@
+var path = require('path');
 var express = require('express');
 var app = express();
 
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'pug');
+
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('index');
 });
 
+app.use(express.static('public'));
+
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('philmanijak.com listening on port 3000');
 });
