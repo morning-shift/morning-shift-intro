da3cdecbe8e2c67f3e9309015516cfdbe70649ed
Phil Manijak <github@exclsr.com>
2016-05-06 14:50:37 -0700

    Config: Read Stripe key from the file system
    ... the private one.

--- app/config-secrets.js
+++ app/config-secrets.js
@@ -1,4 +1,19 @@
-// Don't put secret values nto the source tree.
-module.exports = {
+var fs = require('fs');
+module.exports = function () {
 
+    var stripeKeyPath = '/home/ubuntu/stripe/private.key';
+
+     try {
+        var values = {
+            stripePrivateKey: fs.readFileSync(stripeKeyPath)
+        };  
+    }
+    catch (err) {
+        console.log(err);
+        return {
+            stripePrivateKey: ""
+        }
+    }
+
+    return values;
 };
