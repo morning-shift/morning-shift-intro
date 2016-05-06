9421a455569b9e2270e77699ac1cca3e9a295452
Phil Manijak <github@exclsr.com>
2016-05-05 21:46:05 -0700

    Add https server
    Just the basics.

---
+++ app/https-server.js
@@ -0,0 +1,37 @@
+// Basic https server with hard-coded paths.
+//
+var https = require('https');
+var fs    = require('fs');
+
+var httpsServer = undefined;
+
+module.exports = function (app) {
+
+    var sslKeyPath    = '/home/ubuntu/ssl/philmanijak.key';
+    var sslCertPath   = '/home/ubuntu/ssl/philmanijak.cert';
+    var sslCaPath     = '/home/ubuntu/ssl/GandiStandardSSLCA2.pem';
+    var sslCaPathRoot = '/home/ubuntu/ssl/USERTrust.pem';
+
+    try {
+        var options = {
+            key: fs.readFileSync(sslKeyPath),
+            cert: fs.readFileSync(sslCertPath),
+            ca: [
+                fs.readFileSync(sslCaPath),
+                fs.readFileSync(sslCaPathRoot)
+            ]
+        };  
+
+        httpsServer = https.createServer(options, app);
+    }
+    catch (err) {
+        console.log(err);
+        return {
+            listen: function () {
+                // no op.
+                console.log("No https server configured.");
+            }
+        }
+    }
+    return httpsServer;
+};
--- app/index.js
+++ app/index.js
@@ -1,17 +1,29 @@
 var path = require('path');
+var https = require('./https-server.js');
+
 var express = require('express');
 var app = express();
+
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'pug');
 
 
 app.get('/', function (req, res) {
-  res.render('index');
+    res.render('index');
 });
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, function () {
-  console.log('philmanijak.com listening on port 3000');
+    console.log('philmanijak.com http on port 3000');
+});
+
+https(app).listen(4000, function (err) {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log('philmanijak.com https on port 4000');
+    }
 });
