30d6ef29b6f88addc9db55dd51e99c60ca589e5b
Phil Manijak <github@exclsr.com>
2017-02-19 17:53:38 -0800

    Facebook: Use public app

--- app/config.js
+++ app/config.js
@@ -2,6 +2,6 @@ var overrides = require('./config-overrides.js');
 // For publicly viewable configuration.
 module.exports = { 
 	clefPublicKey: overrides.clefPublicKey || "db5b5d2cdd53e9ed9abba7d4d4df685f",
-	facebookAppId: overrides.facebookAppId || "385805705132348"
+	facebookAppId: overrides.facebookAppId || "381391302240455"
     // stripePublicKey: "pk_live_..."
 };
